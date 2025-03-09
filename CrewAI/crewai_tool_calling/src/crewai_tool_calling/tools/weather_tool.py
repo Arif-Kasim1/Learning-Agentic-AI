67ab3effadc06d670fdd160ddd8f37a5e807b647
Arif-Kasim <arif.rozani@gmail.com>
2025-03-09 20:11:53 +0500

    Update weather_tool.py

--- CrewAI/crewai_tool_calling/src/crewai_tool_calling/tools/weather_tool.py
+++ CrewAI/crewai_tool_calling/src/crewai_tool_calling/tools/weather_tool.py
@@ -3,4 +3,4 @@ from crewai.tools import tool
 @tool("weather_tool")
 def weather_tool(city: str) -> str:
     """Get the weather for a city."""
-    return f"Today in {city} it is 30 degrees celsius. Tomorrow it will be 32 degrees celsius." 
+    return f"Custom Tool in Action: Today in {city} it is 30 degrees celsius. Tomorrow it will be 32 degrees celsius." 
